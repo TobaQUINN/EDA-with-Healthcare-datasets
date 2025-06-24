bde7df2f4afccc4818bafb7f5d66204da04ea787
Amirah Oluwadarasimi Alimi <daraalimi78@gmail.com>
2025-06-24 06:59:55 +0100

    Update Data Cleaning and Preprocessing.py
    Saved cleaned dataset after renaming column for domain understanding. Previous version saved before renaming

--- Data Cleaning and Preprocessing.py
+++ Data Cleaning and Preprocessing.py
@@ -6,12 +6,10 @@ df= pd.read_csv(r'C:\Users\DARA2\Documents\Healthcare Datasets.txt')
 df.dropna(inplace=True)
 df.duplicated().sum()
 df.drop_duplicates(inplace=True)
-df.to_csv('Cleaned_Healthcare_Datasets.csv', index=False)
 
 print(df.tail())
 
 #Renaming columns for domain understanding
-import pandas as pd
 
 data = {'Blood Type': [1, 2, 3], 'Blood Group': [4, 5, 6]}
 df = pd.DataFrame(data)
@@ -19,4 +17,6 @@ df = pd.DataFrame(data)
 # Rename columns
 df.rename(columns={'Blood Type': 'Blood Group'}, inplace=True)
 
-print(df)
+# Saving Dataset
+df.to_csv('Cleaned_Healthcare_Datasets.csv', index=False)
+print(df.head())
